8601f210ff4ef1cce7243fccef77abc0cf8006bd
mv02168 <mv02168@3792c8b4-27b1-4bd4-bfb2-a1a1d707aa3c>
2013-10-09 23:38:38 +0000

    Programmer: Michael Vuong Group: Recreation Outlet (Right Minded) Description: Added the Windows Form Application project for the Recreation Outlet POS

---
+++ Retail/RecreationOutletPOS/InventoryForm.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace RecreationOutletPOS
+{
+    partial class InventoryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReports = new System.Windows.Forms.Button();
+            this.btnInventory = new System.Windows.Forms.Button();
+            this.btnReturns = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.btnSales = new System.Windows.Forms.Button();
+            this.dgvInventory = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInventory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnReports
+            // 
+            this.btnReports.Location = new System.Drawing.Point(405, 12);
+            this.btnReports.Name = "btnReports";
+            this.btnReports.Size = new System.Drawing.Size(125, 59);
+            this.btnReports.TabIndex = 16;
+            this.btnReports.Text = "Reports";
+            this.btnReports.UseVisualStyleBackColor = true;
+            // 
+            // btnInventory
+            // 
+            this.btnInventory.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnInventory.Location = new System.Drawing.Point(274, 12);
+            this.btnInventory.Name = "btnInventory";
+            this.btnInventory.Size = new System.Drawing.Size(125, 59);
+            this.btnInventory.TabIndex = 15;
+            this.btnInventory.Text = "Inventory";
+            this.btnInventory.UseVisualStyleBackColor = false;
+            // 
+            // btnReturns
+            // 
+            this.btnReturns.Location = new System.Drawing.Point(143, 12);
+            this.btnReturns.Name = "btnReturns";
+            this.btnReturns.Size = new System.Drawing.Size(125, 59);
+            this.btnReturns.TabIndex = 14;
+            this.btnReturns.Text = "Returns";
+            this.btnReturns.UseVisualStyleBackColor = true;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Location = new System.Drawing.Point(822, 12);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(87, 59);
+            this.btnSettings.TabIndex = 13;
+            this.btnSettings.Text = "Settings";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            // 
+            // btnSales
+            // 
+            this.btnSales.Location = new System.Drawing.Point(12, 12);
+            this.btnSales.Name = "btnSales";
+            this.btnSales.Size = new System.Drawing.Size(125, 59);
+            this.btnSales.TabIndex = 12;
+            this.btnSales.Text = "Sales";
+            this.btnSales.UseVisualStyleBackColor = true;
+            // 
+            // dgvInventory
+            // 
+            this.dgvInventory.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dgvInventory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInventory.Location = new System.Drawing.Point(12, 77);
+            this.dgvInventory.Name = "dgvInventory";
+            this.dgvInventory.Size = new System.Drawing.Size(897, 431);
+            this.dgvInventory.TabIndex = 17;
+            // 
+            // InventoryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(921, 520);
+            this.Controls.Add(this.dgvInventory);
+            this.Controls.Add(this.btnReports);
+            this.Controls.Add(this.btnInventory);
+            this.Controls.Add(this.btnReturns);
+            this.Controls.Add(this.btnSettings);
+            this.Controls.Add(this.btnSales);
+            this.Name = "InventoryForm";
+            this.Text = "InventoryFormcs";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInventory)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReports;
+        private System.Windows.Forms.Button btnInventory;
+        private System.Windows.Forms.Button btnReturns;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnSales;
+        private System.Windows.Forms.DataGridView dgvInventory;
+
+    }
+}
