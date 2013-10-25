cc5d686503b58d75bd675b1e11d5de142d97cc7c
tm77775 <tm77775@3792c8b4-27b1-4bd4-bfb2-a1a1d707aa3c>
2013-10-25 22:22:17 +0000

    From Tyler on 10-25 - Established connection to Titan and our database. - DataFetcherSetter method added: CreateNewPO - User input fields created: CreateNewPO.cshtml - PurchaseOrderController modified to transfer data from View to Model - Other supporting code

--- cs3750Fall2013/RecreationOutlet/Warehouse/Warehouse Management Project/RecOutletWarehouse/RecOutletWarehouse/Models/DataFetcherSetter.cs
+++ cs3750Fall2013/RecreationOutlet/Warehouse/Warehouse Management Project/RecOutletWarehouse/RecOutletWarehouse/Models/DataFetcherSetter.cs
@@ -1,14 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
+using System.Configuration;
 
 namespace RecOutletWarehouse.Models
 {
     public class DataFetcherSetter
     {
 
+        public void NewPurchaseOrder(int POID, int vendorID, int POCreateBy, DateTime POOrderDate,
+                                     DateTime POEstShipDate, decimal POFreightCost, string POTerms,
+                                     string POComments) {
+            using (SqlConnection thisConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["TitanConnection"].ConnectionString)) {
+                thisConnection.Open();
 
+                using (SqlCommand command = new SqlCommand()) {
+                    command.Connection = thisConnection;
+                    command.CommandText = "EXEC dbo.CreatePurchaseOrder "
+                                              + "@POID, @vendorID, @POCreateBy, @POOrderDate,"
+                                              + "@POEstShipdate, @POFreightCost, @POTerms, @POComments";
+                    command.Parameters.AddWithValue("@POID", POID);
+                    command.Parameters.AddWithValue("@vendorID", vendorID);
+                    command.Parameters.AddWithValue("@POCreateBy", POCreateBy);
+                    command.Parameters.AddWithValue("@POOrderDate", POOrderDate);
+                    command.Parameters.AddWithValue("@POEstShipDate", POEstShipDate);
+                    command.Parameters.AddWithValue("@POFreightCost", POFreightCost);
+                    command.Parameters.AddWithValue("@POTerms", POTerms);
+                    command.Parameters.AddWithValue("@POComments", POComments);
 
+                    command.ExecuteNonQuery();
+
+                    command.Parameters.Clear();
+
+                }//.SqlComand
+            }//.SqlConnection
+        }//.NewPurchaseOrder
+    
     }
 }
