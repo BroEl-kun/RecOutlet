a9736f469b8f9a183e72373e02ae94864bd08ff4
tm77775 <tm77775@3792c8b4-27b1-4bd4-bfb2-a1a1d707aa3c>
2013-10-25 22:22:17 +0000

    From Tyler on 10-25 - Established connection to Titan and our database. - DataFetcherSetter method added: CreateNewPO - User input fields created: CreateNewPO.cshtml - PurchaseOrderController modified to transfer data from View to Model - Other supporting code

--- Warehouse/Warehouse Management Project/RecOutletWarehouse/RecOutletWarehouse/Controllers/PurchaseOrderController.cs
+++ Warehouse/Warehouse Management Project/RecOutletWarehouse/RecOutletWarehouse/Controllers/PurchaseOrderController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RecOutletWarehouse.Models;
+using RecOutletWarehouse.Models.PurchaseOrder;
 
 namespace RecOutletWarehouse.Controllers
 {
@@ -20,6 +22,32 @@ namespace RecOutletWarehouse.Controllers
         public ActionResult CreateNewPO() {
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateNewPO(PurchaseOrder PO) {
+            if (!ModelState.IsValid) {
+                return View(PO);
+            }
+            else {
+                DataFetcherSetter db = new DataFetcherSetter();
+                
+                //TODO: Move the character replacement logic to a
+                //tools class and enhance its functionality
+                string POtoInt = PO.PurchaseOrderId;
+                POtoInt = POtoInt.Replace("-", string.Empty);
+
+                int convertedPO = Convert.ToInt32(POtoInt);
+
+                db.NewPurchaseOrder(convertedPO,
+                    PO.VendorId, PO.CreatedBy,
+                    PO.OrderDate, PO.EstShipDate,
+                    PO.FreightCost, PO.Terms,
+                    PO.Comments);
+
+
+                return View("AddPOLineItem");
+            }
         }
 
         public ActionResult AddPOLineItem() {
