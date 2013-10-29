b546ded6988f04c510a0ec401ec91b3394035c41
ms66958 <ms66958@3792c8b4-27b1-4bd4-bfb2-a1a1d707aa3c>
2013-10-29 17:00:48 +0000

    added vendor model folder

---
+++ cs3750Fall2013/RecreationOutlet/Warehouse/Warehouse Management Project/RecOutletWarehouse/RecOutletWarehouse/Models/Vendor/AddVendor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace RecOutletWarehouse.Models.AddVendor
+{
+    public class AddVendor
+    {
+
+        public int VendorID { get; set; }
+
+        public string VendorName { get; set; }
+
+        public string ContactName { get; set; }
+
+        public string ContactPhone { get; set; }
+
+        public string ContactFax { get; set; }
+
+        public string AltPhone { get; set; }
+
+        public string Address { get; set; }
+
+        public string Website { get; set; }
+    }
+}
