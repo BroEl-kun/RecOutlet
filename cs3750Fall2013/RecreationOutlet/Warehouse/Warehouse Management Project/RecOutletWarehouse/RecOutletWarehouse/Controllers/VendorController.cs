817ae22ea436846efedcbc9b358dd981806445e1
rj31273 <rj31273@3792c8b4-27b1-4bd4-bfb2-a1a1d707aa3c>
2013-10-31 14:53:17 +0000

    added VendorController

---
+++ cs3750Fall2013/RecreationOutlet/Warehouse/Warehouse Management Project/RecOutletWarehouse/RecOutletWarehouse/Controllers/VendorController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RecOutletWarehouse.Models.AddVendor;
+using RecOutletWarehouse.Models;
+
+namespace RecOutletWarehouse.Controllers
+{
+    public class VendorController : Controller
+    {
+        //
+        // GET: /Vendor/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddVendor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddVendor(AddVendor vendor)
+        {
+            if (!ModelState.IsValid)
+            {
+                //TODO: replace this view with the correct submit view.
+                return View(vendor);
+            }
+            else
+            {
+                DataFetcherSetter db = new DataFetcherSetter();
+
+                db.AddNewVendor(vendor.VendorId,
+                    vendor.VendorName, vendor.ContactName,
+                    vendor.ContactPhone, vendor.ContactFax,
+                    vendor.AltPhone, vendor.Address,
+                    vendor.Website);
+
+                //TODO: change this to the actual view i need to return like Success! or something.
+                return View("Index");
+            }
+        }
+
+    }
+}
+
