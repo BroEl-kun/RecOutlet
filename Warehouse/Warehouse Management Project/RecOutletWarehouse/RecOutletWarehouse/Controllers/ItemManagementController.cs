9719f771354bd9738202fae18132e590325cd289
rj31273 <rj31273@3792c8b4-27b1-4bd4-bfb2-a1a1d707aa3c>
2013-11-05 15:01:46 +0000

    ItemManagementController

---
+++ Warehouse/Warehouse Management Project/RecOutletWarehouse/RecOutletWarehouse/Controllers/ItemManagementController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RecOutletWarehouse.Models;
+using RecOutletWarehouse.Models.ItemManagement;
+
+
+namespace RecOutletWarehouse.Controllers
+{
+    public class ItemManagementController : Controller
+    {
+        //
+        // GET: /ItemMangement/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
