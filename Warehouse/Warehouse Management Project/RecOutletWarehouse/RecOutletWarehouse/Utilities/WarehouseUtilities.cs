4bdfea77ad8c56696a75692110b4b408812bb348
tm77775 <tm77775@3792c8b4-27b1-4bd4-bfb2-a1a1d707aa3c>
2013-11-14 00:36:02 +0000

    Added WarehouseUtilities to hold various utility methods Implemented RPC Generation

---
+++ Warehouse/Warehouse Management Project/RecOutletWarehouse/RecOutletWarehouse/Utilities/WarehouseUtilities.cs
@@ -0,0 +1,25 @@
+﻿using RecOutletWarehouse.Models.ItemManagement;
+using RecOutletWarehouse.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecOutletWarehouse.Utilities {
+    public class WarehouseUtilities {
+
+        public static long GenerateRPC(Item item) {
+            DataFetcherSetter db = new DataFetcherSetter();
+            //Item convertedItem = db.ConvertNamesToIDs(item); //Convert dept, cat, subcat
+            string RPC = Convert.ToByte(item.Department).ToString("D2"); //better way to do this?
+            RPC += Convert.ToByte(item.Category).ToString("D2");
+            RPC += item.ItemId.ToString("D6");
+            RPC += Convert.ToInt16(item.Subcategory).ToString("D3");
+
+            return Convert.ToInt64(RPC);
+        }
+
+
+
+    }
+}
