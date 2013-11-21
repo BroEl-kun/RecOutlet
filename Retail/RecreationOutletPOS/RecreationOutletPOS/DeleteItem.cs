208f1e11813a379f6ca7af976b7f160f3692ba3e
jn62718 <jn62718@3792c8b4-27b1-4bd4-bfb2-a1a1d707aa3c>
2013-11-21 05:15:19 +0000

    Added quantity popup to deletion that triggers when "Void Item" is clicked. Does not trigger when the delete key is pressed.
    Fixed the checkout form's inability to cancel, and the fact that it cleared the listview.
    
    Fixed awkward deletion caused by sorting, by disabling sorting on the sales form.
    
    Added an error dialog to Void Item and Clear buttons if the listview is empty or an item isn't selected.

---
+++ Retail/RecreationOutletPOS/RecreationOutletPOS/DeleteItem.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecreationOutletPOS
+{
+    public partial class DeleteItem : Form
+    {
+        private SalesForm parent;
+        private int item;
+
+        public DeleteItem(SalesForm parent, int item)
+        {
+            InitializeComponent();
+
+            this.parent = parent;
+            this.item = item;
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            int amount = 0;
+
+            if (tbQuantity.Text == "")
+            {
+                MessageBox.Show("Quantity field can't be left blank.", "Item Void",
+                    MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                tbQuantity.Text = "1";
+            }
+
+            try
+            {
+                int.TryParse(tbQuantity.Text, out amount);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Invalid value in quantity field.", "Item Void",
+                    MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                tbQuantity.Text = "1";
+            }
+
+            parent.voidItem(item, amount);
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tbQuantity_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+                e.Handled = true;
+        }
+    }
+}
