d968fbdf4a1303ced79a42711e6afbed9b5c4b44
mv02168 <mv02168@3792c8b4-27b1-4bd4-bfb2-a1a1d707aa3c>
2013-12-07 21:18:28 +0000

    Programmer: Michael Vuong Group: Recreation Outlet (Right Minded) Description: Added new enums for Store transaction table, and corrected the Enum.Department's MENS_OUTERWEAR to OUTERWEAR to reflect the database's changed value Outerwear

--- Retail/RecreationOutletPOS/RecreationOutletPOS/ShowReportForm.cs
+++ Retail/RecreationOutletPOS/RecreationOutletPOS/ShowReportForm.cs
@@ -8,13 +8,100 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using ReportType = RecreationOutletPOS.Enum.ReportType;
+
 namespace RecreationOutletPOS
 {
     public partial class ShowReportForm : Form
     {
-        public ShowReportForm()
+        /// <summary>
+        /// Programmer: Michael Vuong
+        /// Last Updated: 12/7/2013
+        /// 
+        /// Constructor
+        /// </summary>
+        /// <param name="callingButtonText">Used to determine which report to generate</param>
+        public ShowReportForm(string callingButtonText)
         {
             InitializeComponent();
+
+            determineReportType(callingButtonText);
         }
+
+        #region Reporting Methods
+
+        /// <summary>
+        /// Programmer: Michael Vuong
+        /// Last Updated: 12/7/2013
+        /// 
+        /// Determines which report type to show based on what report button the
+        /// user selected
+        /// </summary>
+        /// <param name="buttonText">
+        /// The text of the calling button used to determine 
+        /// which report to show
+        /// </param>
+        private void determineReportType(string callingButtonText)
+        {
+            try
+            {
+                if (callingButtonText == ReportType.TRANSACTIONS.ToString())
+                    showTransactionReports();
+
+                else if (callingButtonText == ReportType.COMMISSIONS.ToString())
+                    showCommissionReports();
+
+                else
+                    MessageBox.Show("Report type could not be determined", "Unknown Report Call",
+                                     MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// Programmer: Michael Vuong
+        /// Last Updated: 12/4/2013
+        /// 
+        /// Shows a list of all transactions within the date range specified in the
+        /// From and To Date textboxes
+        /// </summary>
+        private void showTransactionReports()
+        {
+            try
+            {
+                
+            }
+
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// Programmer: Michael Vuong
+        /// Last Updated: 12/4/2013
+        /// 
+        /// Shows a list of all commissions within the date range specified in the
+        /// From and To Date textboxes
+        ///< /summary>
+        private void showCommissionReports()
+        {
+            try
+            {
+               
+            }
+
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        #endregion 
     }
 }
