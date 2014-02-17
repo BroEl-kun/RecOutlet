e6b8309e5376d517f4dceb3f15083d885ea696ab
tm77775 <tm77775@3792c8b4-27b1-4bd4-bfb2-a1a1d707aa3c>
2014-02-17 21:06:46 +0000

    Moved PagingInfo to its own file to encourage re-use across multiple sections of the site.
    Added numerous comment headers and in-line comments for documentation.

--- cs3750Fall2013/RecreationOutlet/Warehouse/RecOutletWarehouse/RecOutletWarehouse/HtmlHelpers/PagingHelpers.cs
+++ cs3750Fall2013/RecreationOutlet/Warehouse/RecOutletWarehouse/RecOutletWarehouse/HtmlHelpers/PagingHelpers.cs
@@ -4,14 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using RecOutletWarehouse.Models;
-using RecOutletWarehouse.Controllers;
+using RecOutletWarehouse.Utilities;
 using System.Text;
 
 namespace RecOutletWarehouse.HtmlHelpers {
+    /// <summary>
+    /// PagingHelpers is a static utility class that provides methods for list pagination.
+    /// IMPORTANT: If you want to use any of these methods in your View, make sure you include
+    /// a @using statement at the beginning of the .cshtml file (@using lines must come before
+    /// you declare your model). See BrowseVendors.cshtml for a working implementation.
+    /// </summary>
     public static class PagingHelpers {
 
+        /// <summary>
+        /// Allows you to split a list into numbered pages.
+        /// </summary>
+        /// <param name="html"></param>
+        /// <param name="pagingInfo">A PagingInfo object defining your page attributes.</param>
+        /// <param name="pageUrl">The URL of the list View to be paginated.</param>
+        /// <returns>An HTML string that creates the page links.</returns>
+        /// Changelog
+        ///     Version 1.0 (T.M.)
+        ///         - Initial creation
         public static MvcHtmlString PageLinks(this HtmlHelper html,
-                                              RecOutletWarehouse.Controllers.VendorController.PagingInfo pagingInfo,
+                                              PagingInfo pagingInfo, // PagingInfo is from the Utilities folder
                                               Func<int, string> pageUrl) {
 
             StringBuilder result = new StringBuilder();
@@ -27,6 +43,15 @@ namespace RecOutletWarehouse.HtmlHelpers {
             return MvcHtmlString.Create(result.ToString());
         }
 
+        /// <summary>
+        /// Creates a list of HTML anchor elements corresponding to each letter of the alphabet.
+        /// </summary>
+        /// <param name="html"></param>
+        /// <param name="pageUrl">The URL of the list View to be paginated.</param>
+        /// <returns>An HTML string containg A-Z anchor elements.</returns>
+        /// Changelog
+        ///     Version 1.0 (T.M.)
+        ///         - Initial creation
         public static MvcHtmlString RolodexLinks(this HtmlHelper html, Func<int, string> pageUrl) {
             StringBuilder rolodex = new StringBuilder();
             for (int i = 65; i <= 90; i++) { // Using ASCII decimal equivalents of all capital letters
