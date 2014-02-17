a1fb30a3336a6e39b81c78da36547eabdee29eee
tm77775 <tm77775@3792c8b4-27b1-4bd4-bfb2-a1a1d707aa3c>
2014-02-17 20:25:09 +0000

    Added HtmlHelpers folder and class. This offers the following reusable code:    - PageLinks: a method to create paginated lists.    - RolodexLinks: a method for creating A-Z links and pagination. See the Vendor Management portion of the site to see both in action.
    Updated BrowseVendors and associated controller code to work with above helpers.
    Updated Details / Edit pages.
    Updated CSS file.
    Updated Web.config file.

---
+++ cs3750Fall2013/RecreationOutlet/Warehouse/RecOutletWarehouse/RecOutletWarehouse/HtmlHelpers/PagingHelpers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RecOutletWarehouse.Models;
+using RecOutletWarehouse.Controllers;
+using System.Text;
+
+namespace RecOutletWarehouse.HtmlHelpers {
+    public static class PagingHelpers {
+
+        public static MvcHtmlString PageLinks(this HtmlHelper html,
+                                              RecOutletWarehouse.Controllers.VendorController.PagingInfo pagingInfo,
+                                              Func<int, string> pageUrl) {
+
+            StringBuilder result = new StringBuilder();
+            for (int i = 1; i <= pagingInfo.TotalPages; i++) {
+                TagBuilder tag = new TagBuilder("a"); //this creates <a> elements
+                tag.MergeAttribute("href", pageUrl(i));
+                tag.InnerHtml = i.ToString();
+                if (i == pagingInfo.CurrentPage)
+                    tag.AddCssClass("selected");
+                result.Append(tag.ToString());
+            }
+
+            return MvcHtmlString.Create(result.ToString());
+        }
+
+        public static MvcHtmlString RolodexLinks(this HtmlHelper html, Func<int, string> pageUrl) {
+            StringBuilder rolodex = new StringBuilder();
+            for (int i = 65; i <= 90; i++) { // Using ASCII decimal equivalents of all capital letters
+                TagBuilder tag = new TagBuilder("a");
+                tag.InnerHtml = Convert.ToChar(i).ToString();
+                tag.MergeAttribute("href", pageUrl(i).ToString() + "?firstLetter=" + Convert.ToChar(i));
+                rolodex.Append(tag.ToString());
+
+            }
+
+            return MvcHtmlString.Create(rolodex.ToString());
+        }
+    }
+}
