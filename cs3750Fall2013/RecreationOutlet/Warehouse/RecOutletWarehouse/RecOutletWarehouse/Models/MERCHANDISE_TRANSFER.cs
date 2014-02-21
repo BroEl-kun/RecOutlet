8476ed9423c9fe26032d81d6f3b31094f84e343e
cp56944 <cp56944@3792c8b4-27b1-4bd4-bfb2-a1a1d707aa3c>
2014-02-21 14:48:38 +0000

    Quantity type change

--- cs3750Fall2013/RecreationOutlet/Warehouse/RecOutletWarehouse/RecOutletWarehouse/Models/MERCHANDISE_TRANSFER.cs
+++ cs3750Fall2013/RecreationOutlet/Warehouse/RecOutletWarehouse/RecOutletWarehouse/Models/MERCHANDISE_TRANSFER.cs
@@ -13,7 +13,7 @@ namespace RecOutletWarehouse.Models
         public string TansferComments { get; set; }
         public short TransferCreatedBy { get; set; }
         public System.DateTime TransferCreatedDate { get; set; }
-        public short Quantity { get; set; }
+        public int Quantity { get; set; }
         public byte QtyTypeID { get; set; }
         public virtual ITEM ITEM { get; set; }
         public virtual QTY_TYPE QTY_TYPE { get; set; }
