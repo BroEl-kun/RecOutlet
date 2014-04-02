11e3d8c064afa36ee75d804c46c7a072d0263818
cp56944 <cp56944@3792c8b4-27b1-4bd4-bfb2-a1a1d707aa3c>
2014-04-02 13:51:07 +0000

    Added Validation

--- cs3750Fall2013/RecreationOutlet/Warehouse/RecOutletWarehouse/RecOutletWarehouse/Models/RECEIVING_LOG.cs
+++ cs3750Fall2013/RecreationOutlet/Warehouse/RecOutletWarehouse/RecOutletWarehouse/Models/RECEIVING_LOG.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RecOutletWarehouse.Models
 {
@@ -8,10 +9,14 @@ namespace RecOutletWarehouse.Models
         public int ReceivingID { get; set; }
         public Nullable<int> POLineItemID { get; set; }
         public Nullable<byte> QtyTypeID { get; set; }
+
+        [Required(ErrorMessage="Please select a date")]
         public System.DateTime ReceiveDate { get; set; }
         public string ReceivingNotes { get; set; }
         //If we have a receiving log, we should have an amount...
         //public Nullable<int> ReceivedQty { get; set; }
+
+        [Required(ErrorMessage="Must enter an amount")]
         public int ReceivedQty { get; set; }
         public virtual PO_LINEITEM PO_LINEITEM { get; set; }
     }
